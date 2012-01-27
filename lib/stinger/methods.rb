b187d5f1465770c4256c7748a978928d9b355da4
Daniel Greenlaw <daniel@americanwinery.com>
2012-01-27 13:58:11 -0800

    Extend date parsing to include dates with time zone support

--- lib/stinger/methods.rb
+++ lib/stinger/methods.rb
@@ -2,7 +2,7 @@ module Stinger
   class Methods
     def self.parse_date_options(options)
       options.each do |k,v|
-        options[k] = v.strftime('%Y-%m-%d') if [Date, DateTime, Time].include?(v.class)
+        options[k] = v.strftime('%Y-%m-%d') if [Date, DateTime, Time, ActiveSupport::TimeWithZone].include?(v.class)
       end
 
       options
